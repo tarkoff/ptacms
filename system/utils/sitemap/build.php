6df4d780771b7aca3cab24ef422af32fc1acdab9
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2009-09-13 19:00:06 +0000

    Provide sitemap builder

--- system/utils/sitemap/build.php
+++ system/utils/sitemap/build.php
@@ -1,10 +1,10 @@
 <?php
-require './core/SitemapBuilder.php';
+require dirname(__FILE__) . '/core/SitemapBuilder.php';
 
 $builder = new SitemapBuilder();
 
 $builder->setRootUrl('http://gelezka.net');
-$builder->setSavePath('../../../sitemap.xml');
+$builder->setSavePath(dirname(__FILE__) . '/../../../sitemap.xml');
 $builder->addAllowedExt(array('htm', 'html', 'php'));
 
 $builder->build();
