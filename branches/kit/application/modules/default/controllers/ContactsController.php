69b4bf323ceaa357f863efdba0cafa1202ddbc02
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2010-04-28 10:16:56 +0000

    Add contacts page

---
+++ branches/kit/application/modules/default/controllers/ContactsController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Index Controller
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * @category   KIT
+ * @package    KIT_Core
+ * @copyright  Copyright (c) 2009-2010 KIT Studio
+ * @license    New BSD License
+ * @version    $Id: IndexController.php 330 2010-04-21 17:05:14Z TPavuk $
+ */
+
+class Default_ContactsController extends Zend_Controller_Action
+{
+	public function indexAction()
+	{
+	}
+}
+
