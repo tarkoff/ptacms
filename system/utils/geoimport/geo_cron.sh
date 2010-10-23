0a5d80539d56bf4f4e3fb028e5735663e1966079
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2010-10-23 19:22:36 +0000

    add geotargeting for mixmarket

--- system/utils/geoimport/geo_cron.sh
+++ system/utils/geoimport/geo_cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Doenload compressed xml file
-#wget --output-document=mixml.plx.gz "http://mixmarket.biz/mixml.plx?id=4294945418&zip=1"
+wget --output-document=geo.tar.gz "http://ipgeobase.ru/files/db/Main/db_files.tar.gz"
 
 # unzip xml file
-#gunzip -f mixml.plx.gz
+gunzip -c geo.tar.gz | tar xvf -
 
 iconv -f Windows-1251 -t UTF-8 < cidr_ru_block.txt > cidr_ru_block_utf8.txt
 
