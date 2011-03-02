4bad4619ecc52c45a5bfdf85699853f200fcac00
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-03-02 14:29:17 +0000

    Provide fronted of posts

--- branches/kit/public/js/custom.js
+++ branches/kit/public/js/custom.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var data = ['sup', '.com', 'port', '@', 'evice', 'satd'];
 	var cnct = data[0] + data[2] + data[3] + data[5] + data[4] + data[1];
 	$('#support a, #contacts a').attr('href', 'mailto:' + cnct).text(cnct);
+	
 });
 
 
