1f981351f8949eae41f471542c09ba3146bd3556
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-24 14:10:48 +0000

    fix miximport

--- branches/gelezkaz/data/utils/miximport2/mix_truncate.sh
+++ branches/gelezkaz/data/utils/miximport2/mix_truncate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Doenload compressed xml file
-wget --output-document=advs.plx "http://price.mixmarket.biz/yaml.plx?id=4294945418&list=1"
+wget --output-document=advs_.plx "http://price.mixmarket.biz/yaml.plx?id=4294945418&list=1"
+iconv -f windows-1251 -t UTF-8 advs_.plx > advs.plx
+# rm -f advs.plx 
+# mv advs_.plx advs.plx
 
 # Parse xml file
 php mix_truncate.php > mix_truncate.log
