411b39023de26ffb0406b1ecb6d8f021c3a45a6b
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-24 10:40:45 +0000

    fix mail antispam

--- branches/gelezkaz/public/js/catalogio/custom.js
+++ branches/gelezkaz/public/js/catalogio/custom.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	var data = ['sup', '.net', 'port', '@', 'ezka', 'gel'];
+	var cnct = data[0] + data[2] + data[3] + data[5] + data[4] + data[1];
+	$('#support a, #contacts a').attr('href', 'mailto:' + cnct).text(cnct);
+
 	//Search bar tabs
 	$("#search-tabs").tabs();
 
