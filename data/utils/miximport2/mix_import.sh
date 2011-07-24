1aa1aa262abe20e833610744ca41e1579b4ef865
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-24 14:10:48 +0000

    fix miximport

--- data/utils/miximport2/mix_import.sh
+++ data/utils/miximport2/mix_import.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Download compressed xml file
-# wget --output-document=mixml.plx.gz "http://mixmarket.biz/mixml.plx?id=4294945418&zip=1"
+# wget --output-document=mixml_.plx.gz "http://mixmarket.biz/mixml.plx?id=4294945418&zip=1"
 
 # unzip xml file
-# gunzip -f mixml.plx.gz
+gunzip -f mixml_.plx.gz
+iconv -f windows-1251 -t UTF-8 mixml_.plx > mixml.plx
 
 # Parse xml file
 php mix_import.php > mix_import.log
