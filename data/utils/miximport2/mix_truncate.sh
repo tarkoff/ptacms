ff68fd1561bff0011b2aa88bdef7a96478087479
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-24 17:45:10 +0000

    fix cron jobs

--- data/utils/miximport2/mix_truncate.sh
+++ data/utils/miximport2/mix_truncate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~/app/data/utils/miximport2
+
 # Doenload compressed xml file
 wget --output-document=advs_.plx "http://price.mixmarket.biz/yaml.plx?id=4294945418&list=1"
 iconv -f windows-1251 -t UTF-8 advs_.plx > advs.plx
