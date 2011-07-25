2a6a1978fa57428512395dea5e7dc26cef561403
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-25 18:09:10 +0000

    fix miximport

--- data/utils/miximport2/mix_port.sh
+++ data/utils/miximport2/mix_port.sh
@@ -10,5 +10,4 @@ cd ~/app/data/utils/miximport2
 # iconv -f windows-1251 -t UTF-8 mixml_.plx > mixml.plx
 
 # Parse xml file
-php mix_import.php > mix_import.log
-# php mix_port.php > mix_port.log
+php mix_port.php > mix_port.log
