43685503a84198d40bcabb67cdb9011dad9fc897
TPavuk <TPavuk@acd531f4-d421-11dd-b931-6be3727e431d>
2011-07-30 16:41:42 +0000

    fix miximport

---
+++ branches/gelezkaz/data/utils/miximport/mix_import.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/app/data/utils/miximport
+
+# Download compressed xml file
+# wget --output-document=mixml_.plx.gz "http://mixmarket.biz/mixml.plx?id=4294945418&zip=1"
+
+# unzip xml file
+# gunzip -f mixml_.plx.gz
+# iconv -f windows-1251 -t UTF-8 mixml_.plx > mixml.plx
+
+# Parse xml file
+php mix_import.php > mix_import.log
+# php mix_port.php > mix_port.log
