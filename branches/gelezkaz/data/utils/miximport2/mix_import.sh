e6d7033d38ff78890ae88ccf06b43a1cfbe8ce99
TPavuk@gmail.com <TPavuk@gmail.com@acd531f4-d421-11dd-b931-6be3727e431d>
2013-04-27 08:12:12 +0000

    Improve MixMarket products import

--- branches/gelezkaz/data/utils/miximport2/mix_import.sh
+++ branches/gelezkaz/data/utils/miximport2/mix_import.sh
@@ -10,5 +10,5 @@ cd ~/app/data/utils/miximport2
 # iconv -f windows-1251 -t UTF-8 mixml_.plx > mixml.plx
 
 # Parse xml file
-php mix_import.php > mix_import.log
+php mix_import.php >> mix_import.log
 # php mix_port.php > mix_port.log
