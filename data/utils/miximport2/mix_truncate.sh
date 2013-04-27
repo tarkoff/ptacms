c4909938c9cf5668744334fc3db13bb41c2c1c2b
TPavuk@gmail.com <TPavuk@gmail.com@acd531f4-d421-11dd-b931-6be3727e431d>
2013-04-27 08:12:12 +0000

    Improve MixMarket products import

--- data/utils/miximport2/mix_truncate.sh
+++ data/utils/miximport2/mix_truncate.sh
@@ -5,7 +5,8 @@ cd ~/app/data/utils/miximport2
 # Doenload compressed xml file
  wget --output-document=advs_.plx "http://price.mixmarket.biz/yaml.plx?id=4294945418&list=1"
  iconv -f windows-1251 -t UTF-8 advs_.plx > advs.plx
- rm -f advs_.plx 
+ rm -f advs_.plx
+ rm -f *.log  
 # mv advs_.plx advs.plx
 
 # Parse xml file
